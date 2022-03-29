8b5b89d0bbeae0f7eebb8ddcc3889253ffee3eb7
MojoMoogly574 <jdweese574@gmail.com>
2022-03-28 19:06:29 -0500

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Joseph DeWeese on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
